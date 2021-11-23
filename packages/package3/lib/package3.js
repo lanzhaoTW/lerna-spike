37b1c4f81327d53c248cfa713f2bf37c211aa4f8
lanzhao <lanzhao@thoughtworks.com>
2021-11-23 16:57:31 +0800

    add package1，2 to package3

--- packages/package3/lib/package3.js
+++ packages/package3/lib/package3.js
@@ -1,8 +1,13 @@
 'use strict';
 
+import package1 from '@lan/package1'
+import package2 from '@lan/package2'
+
 module.exports = package3;
 
 function package3() {
+    package1()
+    package2()
     console.log("this is package 3")
 
 }
